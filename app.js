aaa30d02fc234f76e247139305f58a69f626b198
Samerassi123 <12330260@students.liu.edu.lb>
2026-05-22 01:00:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/navbar';
 import Footer from './Components/footer';
 import Home from './pages/home';
